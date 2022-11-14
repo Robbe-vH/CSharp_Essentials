a348ea517876cfbc8aa4c9823a308d5a46d6e056
Robbe vanHoppe <12200496@student.pxl.be>
2022-11-14 12:54:00 +0100

    Les 8 klaar

--- Les_8_WPF_Controls/Les_8_WPF_Controls/MainWindow.xaml.cs
+++ Les_8_WPF_Controls/Les_8_WPF_Controls/MainWindow.xaml.cs
@@ -24,9 +24,57 @@ namespace Les_8_WPF_Controls
         {
             InitializeComponent();
 
-            TxtInput.Text = "regel 1 \n" + "regel 2 \n" + "regel 3";
+            // BtnVerwerk.Visibility = Visibility.Collapsed;
+            // Paneel.Background = Brushes.PeachPuff;
+
+            // TxtInput.Text = "regel 1 \n" + "regel 2 \n" + "regel 3";
         }
 
+        private void MnItemFill_Click(object sender, RoutedEventArgs e)
+        {
+            ImgCornflakes.Stretch = Stretch.Fill;
+        }
 
+        private void MnItemStretch_Click(object sender, RoutedEventArgs e)
+        {
+            ImgCornflakes.Stretch = Stretch.UniformToFill;
+        }
+
+        private void MnItemFit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //private void BtnVeranderFoto_Click(object sender, RoutedEventArgs e)
+        //{
+        //    ImgVerandering.Source = new BitmapImage(new Uri("/assets/cornflakes.jpeg", UriKind.Relative));
+
+        //}
+
+        //private void BtnVerwerk_Click(object sender, RoutedEventArgs e)
+        //{
+        //    // Property IsChecked kan null zijn, daarom "== true/false" check doen
+        //    if (CheckVIP.IsChecked == true)
+        //    {
+        //        MessageBox.Show("Welcome to VIP section");
+        //    } else {
+        //        MessageBox.Show("Welome to normal section");
+        //    }
+
+        //    if (RdbtnMan.IsChecked == true)
+        //    {
+        //        MessageBox.Show("man");
+        //    }
+        //    else if (RdbtnVrouw.IsChecked == true)
+        //    {
+        //        MessageBox.Show("vrouw");
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("onbekend");
+        //    }
+
+        //    Paneel.Background = Brushes.MediumTurquoise;
+        //}
     }
 }
