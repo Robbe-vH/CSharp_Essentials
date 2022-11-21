a269eb51e226a88f0de05d285ced4e3a584689ae
Robbe vanHoppe <12200496@student.pxl.be>
2022-11-21 12:47:18 +0100

    Les 9 klaar

---
+++ Les_9_FoutAfhandeling/Les_9_FoutAfhandeling/Program.cs
@@ -0,0 +1,62 @@
+﻿static void Uitleg()
+{
+    Console.Write("Geef ee ngettal om te verdubbelen: ");
+
+    string input = Console.ReadLine();
+    int getal = 0;
+
+    try
+    {
+        getal = Convert.ToInt32(input);
+    }
+    catch (FormatException e)
+    {
+        Console.WriteLine("De input was geen getal, default waarde 0 wordt gebruikt");
+        Console.WriteLine(e.Message);
+    }
+    catch (DivideByZeroException e)
+    {
+        Console.WriteLine("De meest specifieke exception van arithmics");
+        Console.WriteLine(e.Message);
+    }
+    catch (SystemException e)
+    {
+        Console.WriteLine("Algemener dan divide by 0");
+        Console.WriteLine(e.Message);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Meest algemene fout");
+        Console.WriteLine(e.Message);
+    }
+    finally
+    {
+        Console.WriteLine($"het verdubbelde getal is: {getal * 2}");
+        Console.WriteLine("Fijne dag nog");
+    }
+}
+
+
+static int VraagInputVangetal(string vraag)
+{
+    Console.WriteLine(vraag);
+    int getal;
+    if (int.TryParse(Console.ReadLine(), out getal))
+    {
+        return getal;
+    }
+    else
+    {
+        throw new FormatException("De ingegeven waarde is geen getal!");
+    }
+}
+try
+{
+    int gebruikerGetal = VraagInputVangetal("Geef een getal: ");
+}
+catch (FormatException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
---
+++ Oefening_22_Dobbelsteen/Oefening_22_Dobbelsteen/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oefening_22_Dobbelsteen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Random random;
+        private StringBuilder sb;
+        public MainWindow()
+        {
+            InitializeComponent();
+            random = new Random();
+            sb = new StringBuilder();
+        }
+
+
+        private void BtnStart_Click(object sender, RoutedEventArgs e)
+        {
+            int resultaat = random.Next(1, 7);
+            int aantalWorpen = 1;
+
+            while (resultaat < 6) {
+                resultaat = random.Next(1, 7);
+                sb.AppendLine($"Worp {aantalWorpen} is: {resultaat}");
+                aantalWorpen++;
+            }
+
+            TxtOutput.Text = sb.ToString();
+        }
+
+        private void BtnOpnieuw_Click(object sender, RoutedEventArgs e)
+        {
+            TxtOutput.Clear();
+            sb.Clear();
+            BtnStart.Focus();
+        }
+
+        private void BtnAfsluiten_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
