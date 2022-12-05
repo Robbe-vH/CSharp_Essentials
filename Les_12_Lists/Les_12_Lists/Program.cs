a747d2e1e0022b1512d04db24c237ba40a262718
Robbe vanHoppe <12200496@student.pxl.be>
2022-12-05 11:56:58 +0100

    Les 11 Lists

--- Les_12_Lists/Les_12_Lists/Program.cs
+++ Les_12_Lists/Les_12_Lists/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// Declaratie
+
+//List<string> studenten = new List<string>() { };
+//List<double> scores = new List<double>();
+
+//// lijst met standaardwaardes
+//List<string> vakken = new List<string>() { "c#", "Web", "Nederlands"};
+
+//// Index notatie
+//Console.WriteLine(vakken[2]);
+
+//foreach (var vak in vakken)
+//{
+//    Console.WriteLine(vak);
+//}
+
+
+////methodes
+
+////toevoegen
+//vakken.Add("Frans");
+
+//// clear
+//vakken.Clear();
+
+////AddRange
+//string[] DVOVakken = new string[] { "Photoshop", "Lightroom" };
+//vakken.AddRange(DVOVakken);
+
+////Contains
+//if (vakken.Contains("WO3"))
+//{
+//    Console.WriteLine("We zijn in de toekomst");
+//}
+
+//// insert voegt iets toe op een specifieke plaats
+//vakken.Insert(4,"WO2");
+
+////Remove een specifieke string
+//vakken.Remove("Lightroom");
+
+////RemoveAt op een index
+//vakken.RemoveAt(4);
+
+//List<string> oorlogen = vakken.GetRange(4, 2); // start vanaf index 4, gaat dan 2 elementen verder
+
+////sort reverse
+//vakken.Sort();
+//vakken.Reverse();
+
+////indexOf
+//int indexVanWeb = vakken.IndexOf("Web");
+//Console.WriteLine(indexVanWeb);
+
+////Count: geeft de lengte van list *Geen methode*
+//int lengte = vakken.Count;
+
+
+//Opdracht 2
+
+List<string> favoSeries = new List<string>();
+bool antw = true;
+do
+{
+    Console.Write("Geef een favoriete serie: ");
+    favoSeries.Add(Console.ReadLine());
+    Console.Write("Wilt u nog een serie ingeven? (y/n) ");
+    if (Console.ReadLine() == "y")
+    {
+        antw = true;
+    }
+    else
+    {
+        antw = false;
+        foreach (var serie in favoSeries)
+        {
+            Console.WriteLine(serie);
+        }
+    }
+} while (antw);
