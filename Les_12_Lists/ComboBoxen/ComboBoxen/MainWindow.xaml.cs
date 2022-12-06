fd6c22cacc06cb0961db13851bc59b51f50527af
Robbe vanHoppe <12200496@student.pxl.be>
2022-12-06 12:02:29 +0100

    Les over Listboxes en Comboboxes

---
+++ Les_12_Lists/ComboBoxen/ComboBoxen/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ComboBoxen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ComboBoxItem comboBoxItem = new ComboBoxItem();
+            comboBoxItem.Content = "C#";
+            CmbxVakken.Items.Add(comboBoxItem);
+
+            ComboBoxItem comboBoxItem1 = new ComboBoxItem();
+            comboBoxItem1.Content = "Web Design";
+            CmbxVakken.Items.Add(comboBoxItem1);
+        }
+
+        private void CmbxVakken_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CmbxVakken.SelectedIndex > -1)
+            {
+                ComboBoxItem selectedItem = (ComboBoxItem)CmbxVakken.SelectedItem;
+                LblVak.Text = selectedItem.Content.ToString();
+            }
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+             CmbxVakken.SelectedIndex = -1;
+            LblVak.Text = string.Empty;
+        }
+    }
+}
---
+++ Les_12_Lists/WPF_Controls_Lijsten/WPF_Controls_Lijsten/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_Controls_Lijsten
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // nooit gewoon een stuk tekst toevoegen, altijd een listboxitem met een content
+            // anders kunnen er mismatchende datatypes komen
+            //LbxNamen.Items.Add("Lorem Ipsum");
+            
+            ListBoxItem item = new ListBoxItem();
+            item.Content = "Yeyeyeyeye";
+            LbxNamen.Items.Add(item);
+        }
+
+        private void BtnVoegToe_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem item = new ListBoxItem();
+            item.Content = TxtNaam.Text;
+            LbxNamen.Items.Add(item);
+
+            TxtNaam.Text = string.Empty;
+        }
+
+        private void BtnVerwijder_Click(object sender, RoutedEventArgs e)
+        {
+            //ListBoxItem[] geselecteerdItems = (ListBoxItem[])LbxNamen.SelectedItems;
+            //StringBuilder rmItems = new StringBuilder();
+            //foreach (ListBoxItem item in geselecteerdItems)
+            //{
+            //    rmItems.AppendLine(item.Content.ToString());
+            //}
+            //LbxNamen.Items.Remove(geselecteerdItems);
+            //MessageBox.Show($"U heeft { rmItems } verwijderd");
+            
+        }
+
+        private void BtnShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (ListBoxItem item in LbxNamen.Items)
+            {
+                sb.AppendLine(item.Content.ToString());
+            }
+            MessageBox.Show(sb.ToString());
+        }
+    }
+}
