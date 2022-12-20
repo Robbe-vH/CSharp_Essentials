06968cfa80bbae465c738acf947fe625ee2ff987
Robbe vanHoppe <12200496@student.pxl.be>
2022-12-20 11:33:54 +0100

    Oefening Eevee Catcher half klaar

--- Oefening_Eevee_Catcher/Oefening_Eevee_Catcher/MainWindow.xaml.cs
+++ Oefening_Eevee_Catcher/Oefening_Eevee_Catcher/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Oefening_Eevee_Catcher
 {
@@ -20,9 +11,92 @@ namespace Oefening_Eevee_Catcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<string, string> eeveeImages = new Dictionary<string, string>
+        {
+            { "Eevee", "/images/Eevee.png" },
+            { "EeveeGigantamax", "/images/EeveeGigantamax.png" },
+            { "Espeon", "/images/Espeon.png" },
+            { "Flareon", "/images/Flareon.png" },
+            { "Glaceon", "/images/Glaceon.png" },
+            { "Jolteon", "/images/Jolteon.png" },
+            { "Leafeon", "/images/Leafeon.png" },
+            { "Sylveon", "/images/Sylveon.png" },
+            { "Umbreon", "/images/Umbreon.png" },
+            { "Vaporeon", "/images/Vaporeon.png" }
+        };
+
+        private DispatcherTimer eeveeTimer;
+        private DispatcherTimer huidigeTijd;
+        private DispatcherTimer highscoreTimer;
+        private int fotoTeller;
+        private BitmapImage eeveeBitmapFoto;
+        private Random willekeurigeFoto;
+        private DateTime startTijd;
+        private DateTime endTijd;
+        private int totaaltijd = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            eeveeTimer = new DispatcherTimer(DispatcherPriority.Normal);
+            eeveeTimer.Tick += new EventHandler(eeveeTimer_Tick);
+            eeveeTimer.Interval = new TimeSpan(0, 0, 1);
+
+            huidigeTijd = new DispatcherTimer();
+            huidigeTijd.Tick += new EventHandler(huidigeTijd_Tick);
+            huidigeTijd.Interval = new TimeSpan(0, 0, 1);
+            huidigeTijd.Start();
+
+            willekeurigeFoto = new Random();
+        }
+
+        // Timers
+        public void eeveeTimer_Tick(object? sender, EventArgs e)
+        {
+            // Namen van de Keys linken aan getallen
+            List<string> keys = new List<string>(eeveeImages.Keys);
+
+            // Een willekeurige foto inladen uit de dictionary
+            fotoTeller = willekeurigeFoto.Next(1, keys.Count);
+            string fotopath = eeveeImages[keys[fotoTeller]];
+
+            // Bitmap foto maken
+            eeveeBitmapFoto = new BitmapImage();
+            eeveeBitmapFoto.BeginInit();
+            eeveeBitmapFoto.UriSource = new Uri(fotopath, UriKind.Relative);
+            eeveeBitmapFoto.EndInit();
+
+            // Bitmap foto linken aan xaml
+            ImgEevee.Source = eeveeBitmapFoto;
+
+            // start tijd score
+            startTijd = DateTime.Now;
+        }
+
+        private void huidigeTijd_Tick(object? sender, EventArgs e)
+        {
+            LblHudigeTijd.Text = DateTime.Now.ToLongTimeString();
+        }
+
+
+        // Knop
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            eeveeTimer.Start();
+        }
+
+        private void ImgEevee_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (fotoTeller == 1)
+            {
+                TimeSpan score = TimeSpan.FromMilliseconds(startTijd - DateTime.Now);
+                eeveeTimer.Stop();
+                MessageBox.Show($"Uw Moederrrrrr {totaaltijd}ms");
+            }
+            else
+            {
+                // scoreTijd += 100ms;
+            }
         }
     }
 }
